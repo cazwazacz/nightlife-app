e942881cf7e25fa1f4ba9aeb36eb5e36884449c3
Allan Wazacz <cazwazacz@gmail.com>
2017-02-17 16:36:53 +0000

    changed callback url for twitter log in

--- app/config/auth.js
+++ app/config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
     'twitterAuth': {
         'consumerKey': process.env.TWITTER_CONSUMER_KEY,
         'consumerSecret': process.env.TWITTER_CONSUMER_SECRET,
-        'callbackURL': 'http://127.0.0.1:3000/' + 'auth/twitter/callback'
+        'callbackURL': 'https://caz-nightlife-app.herokuapp.com/' + 'auth/twitter/callback'
     }
 };
